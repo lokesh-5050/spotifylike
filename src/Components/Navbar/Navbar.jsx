31fde9e553d38f5eda96854c23f7f9e62c21ec9d
Lokesh-mali <102310338+lokesh-5050@users.noreply.github.com>
2023-01-30 16:20:04 +0530

    nav dropdown

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { MdArrowBackIosNew, MdArrowForwardIos } from 'react-icons/md'
-import { RiArrowDownSFill} from 'react-icons/ri'
+import { RiArrowDownSFill } from 'react-icons/ri'
 import module from './Navbar.module.css'
 const Navbar = () => {
+    const dropDownNav = useRef(null)
+    const appendUserOpt = (e)=>{
+        console.log(e)
+    }
     return (
         <>
             <div className={`${module.nav}`}>
@@ -20,12 +24,18 @@ const Navbar = () => {
                     <div className={`${module.btn}`}>
                         <h6>Upgrade</h6>
                     </div>
-                    <div className={`${module.btn} ${module.extra}`}>
-                    <div className={`${module.image}`}>
-                        <img src="" alt="" />
-                    </div>
-                        <h6>Lokesh Mali</h6>
-                        <h5><RiArrowDownSFill fontSize='1.7vw'/></h5>
+
+                    <div onClick={appendUserOpt} ref={dropDownNav} className={`${module.btn} ${module.extra}`}>
+                        <div className={`${module.upper}`}>
+                            <div className={`${module.image}`}>
+                                <img src="https://i.scdn.co/image/ab67706f00000002b7d25e59b0f3847ac0a66412" alt="" />
+                            </div>
+                            <h6>Lokesh Mali</h6>
+                            <h5><RiArrowDownSFill fontSize='1.7vw' /></h5>
+                        </div>
+                            <div className={`${module.down}`}>
+                                <h6>Lokesh Mali</h6>
+                            </div>
                     </div>
                 </div>
             </div>
